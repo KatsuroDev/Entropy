5cb48f9fc52d831e08f87e38877f4b2781c7c9c5
Thomas Lessard <thomaslessard122@hotmail.com>
2020-06-12 22:40:55 -0400

    frame_buffer_callback clear screen

--- src/Entropy/Core/Window.cpp
+++ src/Entropy/Core/Window.cpp
@@ -6,10 +6,10 @@ namespace Entropy {
     // Re-drawing the buffer is done by default on resize
     void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     {
-        //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         // make sure the viewport matches the new window dimensions; note that width and
         // height will be significantly larger than specified on retina displays.
-        //glViewport(0, 0, width, height);
+        glViewport(0, 0, width, height);
         // Re-render the scene because the current frame was drawn for the old resolution
         glfwSwapBuffers(window);
     }
